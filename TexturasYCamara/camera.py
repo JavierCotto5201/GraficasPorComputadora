82f40884fda73f1d9cd944add3cbe9c852afb72e
JavierCotto5201 <53280525+JavierCotto5201@users.noreply.github.com>
2021-10-03 23:25:29 -0600

    PROYECTO

---
+++ TexturasYCamara/camera.py
@@ -0,0 +1,105 @@
+from gl import *
+import random
+from random import randint
+
+def gourad(render, **kwargs):
+    w, v, u = kwargs['bar']
+    tx, ty = kwargs['tex_coords']
+    nA, nB, nC = kwargs['varying_normales']
+    
+    tcolor = render.current_texture.get_color(tx, ty)
+
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    
+    intensity = w*iA + v*iB + u*iC
+    
+    return tcolor * intensity
+
+def Tie_shader(render, **kwargs):
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normales']
+    A, B, C = kwargs['triangle']
+
+    tcolor = color(57, 59, 168)
+    
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    
+    intensity = w*iA + v*iB + u*iC
+    
+    return tcolor * intensity *2
+
+def Gun_shader(render, **kwargs):
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normales']
+    A, B, C = kwargs['triangle']
+
+    tcolor = color(200, 0, 0)
+    
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    
+    intensity = w*iA + v*iB + u*iC
+    
+    return tcolor * intensity
+
+def Meteor_shading(render, **kwargs):
+    w, v, u = kwargs['bar']
+    nA, nB, nC = kwargs['varying_normales']
+    A, B, C = kwargs['triangle']
+
+    tcolor = color(250, 250, 250)
+    
+    iA, iB, iC = [dot(n, render.light) for n in (nA, nB, nC)]
+    
+    intensity = w*iA + v*iB + u*iC
+    
+    return tcolor * intensity
+
+pi = 3.14
+
+def estrellas(render):
+    for x in range(0, 300):
+      render.point(random.randint(0, 990), random.randint(0, 990), white)
+
+r = Renderer(1000, 1000)
+r.lookAt(V3(0, 0, 5), V3(0, 0, 0), V3(0, 1, 0))
+
+
+estrellas(r)
+
+#LUNA BOSS
+r.current_texture = Texture('./models/moon_normals.bmp')
+r.light = V3(0.5, 0, 0.5)
+r.load('./models/MOONF.obj', (-0.7, -0.7, -1), (0.5, 1.25, 0.25), (0, 0, 0))
+r.active_shader = gourad
+r.draw_arrays('TRIANGLES')
+
+#TIE FIGHTER
+r.current_texture = None
+r.light = V3(0, 1, 0)
+r.load('./models/Tie_FighterF.obj', (0.65, 0.5, -2), (0.20, 0.20, 0.20), (0.5, -pi/5, 0))
+r.active_shader = Tie_shader
+r.draw_arrays('TRIANGLES')
+
+#TIE FIGHTER
+r.current_texture = None
+r.light = V3(0, 1, 0)
+r.load('./models/Tie_FighterF.obj', (0.35, -0.5, -1), (0.20, 0.20, 0.20), (0, -pi, 0))
+r.active_shader = Tie_shader
+r.draw_arrays('TRIANGLES')
+
+#MOON GUN
+r.current_texture = None
+r.light = V3(0, 0, 1)
+r.load('./models/alien_gun.obj', (-0.8, -0.2, 0), (0.2, 0.2, 0.2), (0, 0, 0))
+r.active_shader = Gun_shader
+r.draw_arrays('TRIANGLES')
+
+#METEOR
+r.current_texture = None
+r.light = V3(-0.5, 0.5, 0)
+r.load('./models/A2Fobj.obj', (0, 0, -10), (1, 1, 1), (0, -10, 0))
+r.active_shader = Meteor_shading
+r.draw_arrays('TRIANGLES')
+
+
+r.render()
---
+++ TexturasYCamara/obj.py
@@ -0,0 +1,92 @@
+import struct
+from lib import *
+
+class Obj(object):
+    def __init__(self, filename):
+        with open(filename) as f:
+            self.lines = f.read().splitlines()
+
+        self.vertices = []
+        self.tvertices = []
+        self.normales = []
+        self.faces = []
+        self.read()
+
+    def read(self):
+        maxy = 0
+        maxx = 0
+        maxz = 0
+        for line in self.lines:
+            if line:
+                try:
+                    prefix, value = line.split(' ', 1)
+                except:
+                    prefix = ''
+                if prefix == 'v':
+                    vertice = list(map(float, value.split(' ')))
+                    self.vertices.append(vertice)
+                    if vertice[0]>maxx:
+                        maxx = vertice[0]
+                    if vertice[1]>maxy:
+                        maxy = vertice[1]
+                    if vertice[2]>maxz:
+                        maxz = vertice[2
+                        ]
+                        
+                elif prefix == 'vt':
+                    self.tvertices.append(
+                      list(map(float, value.split(' ')))
+                    )
+                elif prefix == 'vn':
+                    self.normales.append(
+                      list(map(float, value.split(' '))) 
+                    )
+                elif prefix == 'f':
+                    self.faces.append(
+                        [list(map(int, face.split('/'))) for face in value.split(' ')]
+                    )
+
+        self.vertexN = [[i[0]/maxx, i[1]/maxy, i[2]/maxz] for i in self.vertices]
+        self.vertices = self.vertexN
+
+class Texture(object):
+    def __init__(self, path):
+        self.path = path
+        self.read()
+    
+    def read(self):
+        image = open(self.path, 'rb')
+        
+        image.seek(10)
+        headerSize = struct.unpack('=l', image.read(4))[0]
+
+        image.seek(18)
+        self.width = struct.unpack('=l', image.read(4))[0]
+        self.height = struct.unpack('=l', image.read(4))[0]
+        image.seek(headerSize)
+        self.pixels = []
+        
+        for y in range(self.height):
+            self.pixels.append([])
+            for x in range(self.width):
+                b = ord(image.read(1))
+                g = ord(image.read(1))
+                r = ord(image.read(1))
+                self.pixels[y].append(color(r, g, b))
+        image.close()
+        
+    def get_color(self, tx, ty):
+        x = int(tx * self.width)
+        y = int(ty * self.height)
+        try:
+            return self.pixels[y][x]
+        except:
+            return color(255, 255, 255)
+        
+
+    
+    
+    
+    
+    
+    
